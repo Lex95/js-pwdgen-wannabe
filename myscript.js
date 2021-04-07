dc246f1e661e60542fe5e405fc0327cac1cd9555
Lex95 <andrea.lega1995@gmail.com>
2021-04-07 17:27:12 +0200

    correzione con file js a parte e utilizzo di getElementById

---
+++ myscript.js
@@ -0,0 +1,4 @@
+var nome = prompt("Inserisci il tuo nome");
+var cognome = prompt("Inserisci il tuo cognome");
+var colore = prompt("Inserisci il tuo colore preferito");
+document.getElementById("myId").innerHTML= nome + cognome + colore + "21";
